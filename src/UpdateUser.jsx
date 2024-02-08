61e45facba08c2f9783959ebfefc4a21c452d760
Oneth Kahandawa <onethkahandawa@gmail.com>
2024-02-08 15:47:59 +0530

    finishfrontend

--- src/UpdateUser.jsx
+++ src/UpdateUser.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function UpdateUser() {
-  return <div>UpdateUser</div>;
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form>
+          <h2>Update User</h2>
+           <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input type="text" placeholder="Enter Name" className="form-control"></input>
+           </div>
+
+           <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input type="text" placeholder="Enter Email" className="form-control"></input>
+           </div>
+
+           <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input type="text" placeholder="Enter Age" className="form-control"></input>
+           </div>
+
+           <button className="btn btn-success">Update</button>
+        </form>
+      
+      </div>
+    </div>
+  );
 }
 
 export default UpdateUser;
